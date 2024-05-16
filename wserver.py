49f6a3f55fa637436b7ae63c5b51bb825ec7ba8b
ivan <ivan@MacBook-Pro-ivan.local>
2024-05-16 16:03:21 +0300

    first commit

--- wserver.py
+++ wserver.py
@@ -1,33 +1,37 @@
 import asyncio
 import websockets
+import random
 
 cl_list = []
 
 
 async def send_message(message: str):
-    if cl_list:
-        await asyncio.wait([client.send(message) for client in cl_list])
+    for client in cl_list:
+        await client.send(str(message))
 
 
 async def new_client(cl_soket: websockets.WebSocketClientProtocol, path: str):
     cl_list.append(cl_soket)
-    try:
-        async for new_message in cl_soket:
-            await send_message(new_message)
-    except websockets.exceptions.ConnectionClosedError:
-        pass
-    finally:
-        cl_list.remove(cl_soket)
+    while True:
+        await generate_data()
+        new_message = await cl_soket.recv()
+        await send_message(message=new_message)
 
 
 async def start_server():
-    async with websockets.serve(new_client, "0.0.0.0", 8080, ping_interval=None):
-        await asyncio.Future()
+    await websockets.serve(new_client, "0.0.0.0", 8080, ping_interval=None)
+    
 
+def gen():
+    yield random.randint(1, 10000)
 
-async def main():
-    await asyncio.gather(start_server())
+async def generate_data():
+    while True:
+        message = next(gen())
+        await send_message(str(message))
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(start_server())
+    event_loop.run_forever()
